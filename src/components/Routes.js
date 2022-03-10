8694c30eadcaf94c94723ab5780c798a71489ab3
albert <albert@grey-wing.com>
2022-03-10 09:24:49 +0800

    fixed route key bug and enable toggling of route of map

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -4,44 +4,46 @@ import Route from "./Route";
 import { CalculatorContext } from "../context/CalculatorContext";
 
 const Routes = () => {
-  const { result, gwResult, ngResult } = useContext(CalculatorContext);
+  const { result, gwResult, ngResult, useNgRoute, useGPFRoute, useGwRoute } =
+    useContext(CalculatorContext);
   const allRoutes = useMemo(
-    () => [
-      {
-        name: "GPF",
-        path: result,
-        styles: {
-          paint: {
-            "line-color": "#2c1ce6",
-            "line-width": 4,
+    () =>
+      [
+        useGPFRoute && {
+          name: "GPF",
+          path: result,
+          styles: {
+            paint: {
+              "line-color": "#2c1ce6",
+              "line-width": 4,
+            },
           },
         },
-      },
-      {
-        name: "GW Sea Route",
-        path: gwResult,
-        styles: {
-          paint: {
-            "line-color": "#24ed6a",
-            "line-width": 4,
+        useGwRoute && {
+          name: "GW Sea Route",
+          path: gwResult,
+          styles: {
+            paint: {
+              "line-color": "#24ed6a",
+              "line-width": 4,
+            },
           },
         },
-      },
-      {
-        name: "NGraph",
-        path: ngResult,
-        styles: {
-          paint: {
-            "line-color": "#e8335a",
-            "line-width": 4,
-            "line-dasharray": [4, 3],
+        useNgRoute && {
+          name: "NGraph",
+          path: ngResult,
+          styles: {
+            paint: {
+              "line-color": "#e8335a",
+              "line-width": 4,
+              "line-dasharray": [4, 3],
+            },
           },
         },
-      },
-    ],
-    [result, gwResult, ngResult]
+      ].filter(Boolean),
+    [result, gwResult, ngResult, useNgRoute, useGPFRoute, useGwRoute]
   );
-  return allRoutes.map((route, index) => <Route key={index} {...route} />);
+  return allRoutes.map((route, index) => <Route key={route.name} {...route} />);
 };
 
 export default Routes;
